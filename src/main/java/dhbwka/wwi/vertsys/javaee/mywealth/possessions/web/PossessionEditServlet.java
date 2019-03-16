822f1ffa83636a2615b13d7c35d469b378b50c36
Zarpator <strube.jonas@gmail.com>
2019-03-16 16:21:27 +0100

    add possession edit servlet

---
+++ src/main/java/dhbwka/wwi/vertsys/javaee/mywealth/possessions/web/PossessionEditServlet.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright © 2019 David Scheid
+ * Copyright © 2019 Jonas Strube
+ * Copyright © 2019 Tim Bayer
+ * 
+ * Dieser Quellcode ist lizenziert unter einer
+ * Creative Commons Namensnennung 4.0 International Lizenz.
+ */
+package dhbwka.wwi.vertsys.javaee.mywealth.possessions.web;
+
+/**
+ *
+ * @author Jonas Strube
+ */
+public class PossessionEditServlet {
+    
+}

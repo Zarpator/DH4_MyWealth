856efe6b03520a853559f34c8d079d37a7ef45b1
Zarpator <strube.jonas@gmail.com>
2019-03-16 16:32:26 +0100

    implement basic structure of possession edit servlet

--- src/main/java/dhbwka/wwi/vertsys/javaee/mywealth/possessions/web/PossessionEditServlet.java
+++ src/main/java/dhbwka/wwi/vertsys/javaee/mywealth/possessions/web/PossessionEditServlet.java
@@ -8,10 +8,32 @@
  */
 package dhbwka.wwi.vertsys.javaee.mywealth.possessions.web;
 
+import dhbwka.wwi.vertsys.javaee.mywealth.possessions.ejb.PossessionBean;
+import javax.ejb.EJB;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
- *
+ * Servlet zum Hinzufügen, Löschen und Bearbeiten von Possessions
  * @author Jonas Strube
  */
-public class PossessionEditServlet {
+
+@WebServlet(urlPatterns = "/app/possessions/edit/*")
+public class PossessionEditServlet extends HttpServlet {
+    @EJB
+    PossessionBean possessionBean;
     
+    // wird aufgerufen wenn die Seite zum Hinzufügen oder Bearbeiten von Possessions geladen wird
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        //TODO implement call of website
+    }
+    
+    // wird aufgerufen wenn auf der possession_edit.jsp ein Button zum Hinzufügen, Bearbeiten oder Löschen einer Possession geklickt wird
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+        //TODO implement form confirmation
+    }    
 }

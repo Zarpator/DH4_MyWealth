98636e790a15a732801bbc68ec78327408add475
Zarpator <strube.jonas@gmail.com>
2019-03-27 17:34:08 +0100

    implement saving of new poss in servlet

--- src/main/java/dhbwka/wwi/vertsys/javaee/mywealth/possessions/web/PossessionEditServlet.java
+++ src/main/java/dhbwka/wwi/vertsys/javaee/mywealth/possessions/web/PossessionEditServlet.java
@@ -8,15 +8,22 @@
  */
 package dhbwka.wwi.vertsys.javaee.mywealth.possessions.web;
 
+import dhbwka.wwi.vertsys.javaee.mywealth.common.ejb.ValidationBean;
+import dhbwka.wwi.vertsys.javaee.mywealth.common.web.FormValues;
+import dhbwka.wwi.vertsys.javaee.mywealth.common.web.WebUtils;
 import dhbwka.wwi.vertsys.javaee.mywealth.possessions.ejb.PossessionBean;
+import dhbwka.wwi.vertsys.javaee.mywealth.possessions.ejb.PossessionTypeBean;
 import dhbwka.wwi.vertsys.javaee.mywealth.possessions.jpa.Possession;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet zum Hinzufügen, Löschen und Bearbeiten von Possessions
@@ -29,6 +36,12 @@ public class PossessionEditServlet extends HttpServlet {
     @EJB
     private PossessionBean possessionBean;
     
+    @EJB
+    private PossessionTypeBean possessionTypeBean;
+    
+    @EJB
+    private ValidationBean validationBean;
+            
     // wird aufgerufen wenn die Seite zum Hinzufügen oder Bearbeiten von Possessions geladen wird
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -49,10 +62,75 @@ public class PossessionEditServlet extends HttpServlet {
     }
     
     // wird aufgerufen wenn auf der possession_edit.jsp ein Button zum Hinzufügen, Bearbeiten oder Löschen einer Possession geklickt wird
+    
     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) {
-        //TODO implement form confirmation
-    }    
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Angeforderte Aktion ausführen
+        String action = request.getParameter("action");
+
+        if (action == null) {
+            action = "";
+        }
+
+        switch (action) {
+            case "save":
+                this.saveTask(request, response);
+                break;
+            case "delete":
+                // TODO delete possession
+                //this.deleteTask(request, response);
+                break;
+        }
+    }
+    
+    private void saveTask(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Formulareingaben prüfen
+        List<String> errors = new ArrayList<>();
+
+        String possOwner = request.getParameter("poss_owner");
+        String possType = request.getParameter("poss_type");
+        String possValue = request.getParameter("poss_value");
+        String possComments = request.getParameter("poss_comments");
+
+        Possession possession = this.getPossession(request);
+
+        if (possType != null && !possType.trim().isEmpty()) {
+            try {
+                possession.setType(this.possessionTypeBean.findById(Long.parseLong(possType)));
+            } catch (NumberFormatException ex) {
+                // Ungültige oder keine ID mitgegeben
+            }
+        }
+
+        possession.setComments(possComments);
+
+        this.validationBean.validate(possession, errors);
+
+        // Datensatz speichern
+        if (errors.isEmpty()) {
+            this.possessionBean.update(possession);
+        }
+
+        // Weiter zur nächsten Seite
+        if (errors.isEmpty()) {
+            // Keine Fehler: Startseite aufrufen
+            response.sendRedirect(WebUtils.appUrl(request, "/app/tasks/list/"));
+        } else {
+            // Fehler: Formuler erneut anzeigen
+            FormValues formValues = new FormValues();
+            formValues.setValues(request.getParameterMap());
+            formValues.setErrors(errors);
+
+            HttpSession session = request.getSession();
+            session.setAttribute("task_form", formValues);
+
+            response.sendRedirect(request.getRequestURI());
+        }
+    }
 
     private Possession getPossession(HttpServletRequest request) {
         Possession possession = new Possession();

fe77266b0acb779e121fb4abf88341817c183de6
davidscheid <davidscheid99@gmail.com>
2019-04-04 10:14:02 +0200

    add Currency to ClientAnwendung

--- ClientAnwendung/Controller.js
+++ ClientAnwendung/Controller.js
@@ -3,3 +3,7 @@
 function showAllPossessionTypes(){
   window.location.href="https://localhost:8443/MyWealth/api/PossessionTypes";
 }
+
+function showAllCurrencies(){
+  window.location.href="https://localhost:8443/MyWealth/api/Currencies";
+}
